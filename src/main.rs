3a8fff6f79887a5f18d99371333cb99a7e4afed0
Lukasz Ponski <lponski@yahoo.com>
2025-03-08 19:50:21 +0100

    [mcbstm] add display

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 #![no_std]
 #![no_main]
 
+// mod display;
+// use display::*;
+
+use core::fmt::Write;
+use cortex_m::asm::nop;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::adc::{Adc, Temperature};
-use embassy_stm32::peripherals::{ADC1, PA1};
-use embassy_stm32::{adc, bind_interrupts};
+use embassy_stm32::i2c::I2c;
+use embassy_stm32::peripherals::{ADC1, I2C1, PA1};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{adc, bind_interrupts, i2c};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::mutex::Mutex;
 use embassy_time::{Duration, Timer};
+use embedded_graphics::mono_font::iso_8859_1::FONT_10X20;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
     ADC1_2 => adc::InterruptHandler<ADC1>;
+    I2C1_EV => i2c::EventInterruptHandler<I2C1>;
+    I2C1_ER => i2c::ErrorInterruptHandler<I2C1>;
 });
 
 type AdcType = Mutex<ThreadModeRawMutex, Option<Adc<'static, ADC1>>>;
@@ -56,7 +73,7 @@ async fn temp(
             if let Some(adc_ref) = adc_unlocked.as_mut() {
                 let v = adc_ref.read(&mut adc_temp).await;
                 let celcius = convert_to_celcius(v);
-                //info!("Internal temp: {=u16} ({} C)", v, celcius);
+                info!("Internal temp: {=u16} ({} C)", v, celcius);
             }
         }
         //Timer::after_millis(300).await;
@@ -83,11 +100,50 @@ async fn volt(adc: &'static AdcType, vrefint_sample: u16, delay: Duration, mut p
     }
 }
 
+#[embassy_executor::task]
+async fn display_text() {}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
+    //let i2c = I2c::new_blocking(
+    let i2c = I2c::new(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Irqs,
+        p.DMA1_CH6,
+        p.DMA1_CH7,
+        Hertz::khz(400),
+        Default::default(),
+    );
+
+    let interface = I2CDisplayInterface::new(i2c);
+    //let interface = I2CDisplayInterface::new_custom_address(i2c, 0x3C); //I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+
+    display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_10X20)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("text_line1", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    Text::with_baseline("text_line2", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    Timer::after_millis(300).await;
+
+    // ADC
     let adc = Adc::new(p.ADC1);
     {
         *(ADC.lock().await) = Some(adc);
@@ -98,8 +154,6 @@ async fn main(spawner: Spawner) {
     let mut vrefint = ADC.lock().await.as_mut().unwrap().enable_vref();
     let vrefint_sample = ADC.lock().await.as_mut().unwrap().read(&mut vrefint).await;
     let adc_temp = ADC.lock().await.as_mut().unwrap().enable_temperature();
-
-    Timer::after_millis(300).await;
 
     unwrap!(spawner.spawn(temp(
         &ADC,
